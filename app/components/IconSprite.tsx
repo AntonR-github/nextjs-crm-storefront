991cac153427b17d4c0c8ecd1c24c0970f380988
Anton <antonr1wk@gmail.com>
2026-08-15 22:43:08 +0300

    Add SVG icon sprite component

---
+++ app/components/IconSprite.tsx
@@ -0,0 +1,14 @@
+export default function IconSprite() {
+  return (
+    <svg className="svg-sprite" aria-hidden="true">
+      <symbol id="icon-lock" viewBox="0 0 24 24"><rect x="5" y="10" width="14" height="11" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3"/></symbol>
+      <symbol id="icon-truck" viewBox="0 0 24 24"><path d="M3 6h11v11H3zM14 10h4l3 3v4h-7z"/><circle cx="7" cy="19" r="2"/><circle cx="18" cy="19" r="2"/></symbol>
+      <symbol id="icon-shield" viewBox="0 0 24 24"><path d="M12 3 20 6v6c0 5-3.4 8-8 9-4.6-1-8-4-8-9V6z"/><path d="m8.5 12 2.2 2.2 4.8-5"/></symbol>
+      <symbol id="icon-bag" viewBox="0 0 24 24"><path d="M5 8h14l-1 13H6z"/><path d="M9 9V6a3 3 0 0 1 6 0v3"/></symbol>
+      <symbol id="icon-blade" viewBox="0 0 24 24"><path d="M4 6h16v5H4zM7 11v7M11 11v7M15 11v7M19 11v7"/></symbol>
+      <symbol id="icon-grip" viewBox="0 0 24 24"><rect x="7" y="3" width="10" height="18" rx="4"/><path d="M10 7h4M10 11h4M10 15h4"/></symbol>
+      <symbol id="icon-battery" viewBox="0 0 24 24"><rect x="3" y="6" width="17" height="12" rx="2"/><path d="M20 10h2v4h-2M11 8l-3 5h4l-2 4"/></symbol>
+      <symbol id="icon-motor" viewBox="0 0 24 24"><circle cx="12" cy="12" r="8"/><circle cx="12" cy="12" r="3"/><path d="M12 4V2M12 22v-2M4 12H2M22 12h-2"/></symbol>
+    </svg>
+  );
+}
